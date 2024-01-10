adb8bd7434ae54c406e6409f03ee07b767aee1f9
unknown <vedican.v44@gmail.com>
2024-01-10 18:27:49 +0530

    green

--- greenmentor/src/components/Home.jsx
+++ greenmentor/src/components/Home.jsx
@@ -89,7 +89,7 @@ export const Home = () => {
                   <path fill-rule="evenodd" clip-rule="evenodd" d="M5 8L12 16L19 8H5Z" fill="white" />
                 </svg>
               </div>
-              <ul className={`dropdown-menu ${WaterisOpen ? '' : 'show'}`}>
+              <ul className={`dropdown-menu ${WaterisOpen ? '' : 'show'}`} >
 
                 <li>
                   <a className="dropdown-item" href='#'>
@@ -191,7 +191,7 @@ export const Home = () => {
           {/* ..................... */}
           <div id='homeMainRightDivSecondDiv' >
             <select className="form-select no-arrow" >
-              <option selected>Show Tline: Mar’23 - Apr’23 |
+              <option selected>Show Tline: Mar21 - Apr21 |
                 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="6" viewBox="0 0 8 6" fill="none">
                   <path d="M2.90573 5.00001L0.305725 2.40001C-0.0109418 2.08335 -0.0816084 1.72101 0.093725 1.31301C0.269058 0.905012 0.581392 0.700679 1.03073 0.700012H6.18072C6.63072 0.700012 6.94339 0.904345 7.11872 1.31301C7.29406 1.72168 7.22306 2.08401 6.90573 2.40001L4.30572 5.00001C4.20572 5.10001 4.09739 5.17501 3.98073 5.22501C3.86406 5.27501 3.73906 5.30001 3.60573 5.30001C3.47239 5.30001 3.34739 5.27501 3.23073 5.22501C3.11406 5.17501 3.00573 5.10001 2.90573 5.00001Z" fill="black" fill-opacity="0.84" />
                 </svg>
@@ -217,9 +217,9 @@ export const Home = () => {
                 </svg>
                 <p className='smallFontinDiv' >contribution to Scope3</p>
               </div>
-              <div>
-                <p>441510.9 CO₂e</p>
-                <p>25% YOY ↑</p>
+              <div style={{fontSize : 'smaller'}}>
+                <p>1510.9 CO₂e</p>
+                <p style={{color : 'red', }}>20% YOY ↑</p>
               </div>
             </div>
             <div className='homeTopBoxElement' >
@@ -237,9 +237,9 @@ export const Home = () => {
                 </svg>
                 <p>to revenue ratio </p>
               </div>
-              <div>
-                <p>323586.2 CO₂e</p>
-                <p>24% YOY ↓</p>
+              <div style={{fontSize : 'smaller'}}>
+                <p>77586.2 CO₂e</p>
+                <p style={{color : 'green' }}>24% YOY ↓</p>
               </div>
             </div>
             <div className='homeTopBoxElement' >
@@ -257,16 +257,16 @@ export const Home = () => {
                 </svg>
                 <p className='smallFontinDiv' >contribution to Scope3</p>
               </div>
-              <div>
-                <p>32% of Scope3</p>
-                <p>25% YOY ↑</p>
+              <div style={{fontSize : 'smaller'}}>
+                <p>36% of Scope3</p>
+                <p style={{color : 'red', }}>25% YOY ↑</p>
               </div>
             </div>
             <div className='homeLastElement' >
               <div>
                 Total number of reached suppliers
               </div>
-              <div>143</div>
+              <div>150</div>
 
             </div>
           </div>
